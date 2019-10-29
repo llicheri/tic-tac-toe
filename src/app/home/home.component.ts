a1aed62171bee1cf9262d40f8c1bc0dd2aaff38f
Lorenzo Licheri <lorenzolicheri@gmail.com>
2019-10-29 13:32:31 +0100

    create home html skeleton

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { GameService } from "../game.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor(private gameService: GameService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
