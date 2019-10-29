fc1640d761f33c08764b84a8bf3c48500e57c087
Lorenzo Licheri <lorenzolicheri@gmail.com>
2019-10-29 13:20:53 +0100

    create empty home and game component with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { GameComponent } from "./game/game.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", pathMatch: "full", component: HomeComponent },
+  { path: "game", pathMatch: "full", component: GameComponent },
+  { path: "", pathMatch: "full", redirectTo: "home" }
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, GameComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
